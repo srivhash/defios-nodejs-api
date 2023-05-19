f82839d4b22788c76f329bc651add83571768e32
Ashutosh Srivastava <132872965+srivhash@users.noreply.github.com>
2023-05-19 08:38:59 +0530

    Merged events and listeners

--- src/events.ts
+++ src/events.ts
@@ -56,3 +56,65 @@ export interface ITokensClaimed {
     username: string
     repositoryAccount: PublicKey
 }
+
+export interface IPullRequestSent {
+    pullSender: PublicKey
+    commits: PublicKey
+    metadataUri: string
+}
+
+export interface IAddCommitToPR {
+    commitCreator: PublicKey
+    commitAccount: PublicKey
+    issueAccount: PublicKey
+    metadataUri: string
+    // objective_start_unix: u64,
+    // objective_creation_unix: u64,
+    // objective_end_unix: u64,
+    // objective_deliverable: ObjectiveDeliverable,
+}
+
+
+export interface IAddChildObjective {
+    parent_account: PublicKey
+    added_by: PublicKey
+}
+
+export interface IAddObjectiveData {
+    objective_title: String,
+    metadataUri: String,
+    objective_public_key: PublicKey,
+    objective_issue: PublicKey,
+}
+
+export interface IAddRoadmapData {
+    roadmap_title: String,
+    roadmap_description_link: String,
+    roadmap_creator: PublicKey,
+    issueAccount: PublicKey
+}
+
+
+export interface IIssueUnstaked {
+    issue_staker: PublicKey
+    issue_staker_token_account: PublicKey
+    issueAccount: PublicKey
+    unstaked_amount: number
+    rewards_mint: PublicKey
+    issue_contribution_link: String
+}
+
+export interface IPullRequestAccepted {
+    pull_request_addr: PublicKey,
+    repository: PublicKey,
+    repository_name: String,
+    issue: PublicKey,
+    repository_creator: PublicKey,
+}
+
+
+export interface IVestingScheduleChanged {
+    repository_account: PublicKey,
+    repository_creator: PublicKey,
+}
+
--- src/listeners.ts
+++ src/listeners.ts
@@ -1,5 +1,5 @@
 import * as anchor from '@project-serum/anchor'
-import { Defios } from '../type-file/defios'
+import { Defios } from './defios'
 import {
     ICommitAdded,
     IIssueCreated,
@@ -9,6 +9,18 @@ import {
     ITokensClaimed,
     IVerifiedUserAdded,
 } from './events'
+
+import{
+    IAddObjectiveData,
+    IAddRoadmapData,
+    IAddChildObjective,
+    IPullRequestSent,
+    IAddCommitToPR,
+    IPullRequestAccepted,
+    IVestingScheduleChanged,
+    IIssueUnstaked,
+} from './events2'
+
 import { commitCreated } from './handlers/commitCreated'
 import { issueCreated } from './handlers/issueCreated'
 import { issueStaked } from './handlers/issueStaked'
@@ -16,6 +28,15 @@ import { issueUnstaked } from './handlers/issueUnstaked'
 import { repositoryCreated } from './handlers/repositoryCreated'
 import { addVerifiedUser } from './handlers/verifiedUserAdded'
 import { tokensClaimed } from './handlers/tokensClaimed'
+
+import {addObjectiveData}   from './handlers/addObjectiveData'
+import {addRoadmapData}     from './handlers/addRoadmapData'
+import {addChildObjective}  from './handlers/addChildObjective'
+import {addCommitToPR}      from './handlers/addCommitToPR'
+import {pullRequestSent}    from './handlers/pullRequestSent'
+import {pullRequestAccepted} from './handlers/pullRequestAccepted'
+import {vestingScheduleChanged} from './handlers/vestingScheduleChanged'
+// import {issueUnstaked} from './handlers2/issueUnstaked'
 
 export const addEventListener = (program: anchor.Program<Defios>) => {
     program.addEventListener('NameRouterCreated', (res: INameRouterCreated) => {
@@ -91,6 +112,81 @@ export const addEventListener = (program: anchor.Program<Defios>) => {
             .catch((e) => {
                 console.log('Error Adding claim token: ', e)
             })
+    })
+
+    //New Listeners 
+    program.addEventListener('ObjectiveDataAdded', (res: IAddObjectiveData) => {
+        addObjectiveData(res)
+            .then(() => {
+                console.log('ObjectiveDataAdded')
+            })
+            .catch((e) => {
+                console.log('Error Adding Objective Data: ', e)
+            })
+    })
+    
+
+    program.addEventListener('RoadmapDataAdded', (res: IAddRoadmapData) => {
+        addRoadmapData(res)
+            .then(() => {
+                console.log('RoadmapDataAdded')
+            })
+            .catch((e) => {
+                console.log('Error Adding Roadmap Data: ', e)
+            })
+    })
+
+    program.addEventListener('ChildObjectiveAdded', (res: IAddChildObjective) => {
+        addChildObjective(res)
+            .then(() => {   
+                console.log('ChildObjectiveAdded')
+            })
+            .catch((e) => {
+                console.log('Error Adding Child Objective: ', e)
+            })
+    })
+
+    program.addEventListener('PullRequestSent', (res: IPullRequestSent) => {
+        pullRequestSent(res)
+            .then(() => {
+                console.log('PullRequestSent')
+            })
+            .catch((e) => {
+                console.log('Error Adding Pull Request: ', e)
+
+            })
+    })
+
+    program.addEventListener('CommitAddedToPullRequest', (res: IAddCommitToPR) => {
+        addCommitToPR(res)
+            .then(() => {
+                console.log('CommitAddedToPullRequest')
+            })
+            .catch((e) => {
+                console.log('Error Adding Commit to Pull Request: ', e)
+            })
+    })
+
+    program.addEventListener('PullRequestAccepted', (res: IPullRequestAccepted) => {
+        pullRequestAccepted(res)
+
+            .then(() => {
+                console.log('PullRequestAccepted')
+            })
+            .catch((e) => {
+                console.log('Error Accepting Pull Request: ', e)
+            })
+    })
+
+    program.addEventListener('VestingScheduleChanged', (res: IVestingScheduleChanged) => {
+        vestingScheduleChanged(res)
+            .then(() => {
+                console.log('VestingScheduleChanged')
+            })
+            .catch((e) => {
+                console.log('Error Changing Vesting Schedule: ', e)
+            })
+
     })
 
     console.log('Listeners Added')
