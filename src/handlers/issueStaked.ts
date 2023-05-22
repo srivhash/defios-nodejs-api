548bdc67dde9f5a02317028b4be45208146c0179
toshua <ashutoshsrivastava.as@gmail.com>
2023-05-22 15:20:21 +0530

    Issue Staked

--- src/handlers/issueStaked.ts
+++ src/handlers/issueStaked.ts
@@ -5,39 +5,39 @@ import { Contribution, User } from '../models/users'
 
 export const issueStaked = async (res: IIssueStaked) => {
     return new Promise(async (resolve, reject) => {
-        const user = await User.findOne({
-            user_phantom_address: res.issueStaker.toString(),
-        })
-        if (!user) {
-            reject('User not found')
-            return
-        }
-        const issue = await Issues.findOne({
-            issue_account: res.issueAccount.toString(),
-        })
-        if (!issue) {
-            reject('issue not found')
-            return
-        }
-        issue.issue_stake_amount =
-            parseInt(res.stakedAmount.toString()) +
-            parseInt(issue.issue_stake_amount.toString())
-        issue.save()
-        const contribution = new Contribution({
-            contributor_github: user.user_github,
-            contribution_link: '', // TODO: Add link to contribution
-            contribution_timestamp: new Date(),
-            contribution_amt: res.stakedAmount,
-            contribution_token_symbol: issue.issue_stake_token_symbol,
-            contribution_token_url: issue.issue_stake_token_url,
-            contribution_type: 'inbound',
-        })
-        user.user_contributions.push(contribution)
-        user.save((err, user) => {
-            if (err) {
-                reject(err)
+        try {
+            const user = await User.findOne({
+                user_phantom_address: res.issue_staker.toString(),
+            })
+            if (!user) {
+                reject('User not found')
+                return
             }
-            resolve(user)
-        })
+            const issue = await Issues.findOne({
+                issue_account: res.issue_account.toString(),
+            })
+            if (!issue) {
+                reject('issue not found')
+                return
+            }
+            issue.issue_stake_amount =
+                parseInt(res.staked_amount.toString()) +
+                parseInt(issue.issue_stake_amount.toString())
+            issue.save()
+            const contribution = new Contribution({
+                contributor_github: user.user_github,
+                contribution_link: '', // TODO: Add link to contribution
+                contribution_timestamp: new Date(),
+                contribution_amt: res.staked_amount.toString(),
+                contribution_token_symbol: issue.issue_stake_token_symbol,
+                contribution_token_url: issue.issue_stake_token_url,
+                contribution_type: 'inbound',
+            })
+            user.user_contributions.push(contribution)
+            user.save()
+        }
+        catch (err) {
+            reject(err)
+        }
     })
 }
